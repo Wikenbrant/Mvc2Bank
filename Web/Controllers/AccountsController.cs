b37840b192cee0c2318f013e95147206259ba53d
Wikenbrant <55382118+Wikenbrant@users.noreply.github.com>
2020-05-20 15:08:11 +0200

    missat account/me

--- Web/Controllers/AccountsController.cs
+++ Web/Controllers/AccountsController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Threading.Tasks;
 using Application.Accounts.Queries.GetAccount;
 using Application.Transactions.Queries.GetTransactionsPagination;
+using Infrastructure.Identity;
 using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers
@@ -12,10 +14,12 @@ namespace Web.Controllers
     public class AccountsController : ApiController
     {
         private readonly IMediator _mediator;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public AccountsController(IMediator mediator)
+        public AccountsController(IMediator mediator,UserManager<ApplicationUser> userManager)
         {
             _mediator = mediator;
+            _userManager = userManager;
         }
 
         [HttpGet]
@@ -25,5 +29,9 @@ namespace Web.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<AccountViewModel>> Get([FromRoute]GetAccountQuery query) =>
             await _mediator.Send(query).ConfigureAwait(false);
+
+        [HttpGet("/me")]
+        public async Task<ActionResult<TransactionsPaginationViewModel>> Me() =>
+            Ok(await _userManager.GetUserAsync(User).ConfigureAwait(false));
     }
 }
